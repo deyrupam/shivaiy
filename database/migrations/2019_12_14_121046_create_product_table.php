a18203c050d93f35995046fc2406223341b01358
deyrupam <rupamdey161@gmail.com>
2019-12-14 19:44:51 +0530

    Commit All Files

---
+++ database/migrations/2019_12_14_121046_create_product_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('category_id');
+            $table->string('product_name');
+            $table->string('product_short_desc');
+            $table->string('product_desc');
+            $table->float('product_weight');
+            $table->string('product_sku');
+            $table->string('product_org_price');
+            $table->string('product_selling_price');
+            $table->integer('product_stock');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
---
+++ database/migrations/2019_12_14_123344_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('order_amount');
+            $table->string('order_shipping_address');
+            $table->integer('order_phone');
+            $table->string('order_date');
+            $table->string('order_shipped');
+            $table->string('order_tracking_number');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
